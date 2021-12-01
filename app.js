8bf706ae72d5a68442525f735285d0ff93982225
SEN-SENSATION <prawich.th@gmail.com>
2021-12-01 15:09:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const routes = require("./routes/index-route");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(routes);
 
